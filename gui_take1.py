69875b4d09711e47a9b8e27a4eb39bd05f400a27
AnnahGrace01 <114828612+AnnahGrace01@users.noreply.github.com>
2022-10-01 19:25:37 +0100

    Add files via upload

---
+++ gui_take1.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import random
+import time
+
+root = Tk()
+root.title("Competitive Reaction Time Game")
+root.geometry('2000x1125')
+root.configure(background="black")
+
+# my_frame = Frame(root, bg="black")
+# my_frame.pack(pady=20)
+# title_font = ("Helvetica", 40, "bold")
+# body_font = ("Helvetica", 30)
+# blast_font = ("Helvetica", 40)
+
+pack(
+    input("Welcome: "),
+    player_1 = input("Player one: "),
+    player_2 = input("Player two: ")
+)
+
+root.mainloop()
